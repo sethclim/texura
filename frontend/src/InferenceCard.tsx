e1cb0353f103c29c1587b72c20a23216387729dd
sethclim <sethclim@gmail.com>
2025-06-03 22:51:57 -0400

    working upload of texture to bucket

--- frontend/src/InferenceCard.tsx
+++ frontend/src/InferenceCard.tsx
@@ -5,36 +5,37 @@ export const InferenceCard = () => {
 
     const [prompt, setPrompt] = useState("")
 
-    const requestTexture = () => {
-        console.log("prompt " + prompt)
-    }
-
-    const { isPending, error, data, isFetching } = useQuery({
+    const { isPending, error, data, isFetching, refetch } = useQuery({
         queryKey: ['repoData'],
         queryFn: async () => {
             const response = await fetch('http://localhost:7070/inference', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                // your POST payload here
-                prompt: 'yourData',
-            }),
-            mode: "cors",
-        });
-
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    prompt: prompt,
+                }),
+                mode: "cors",
+            });
+            
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
-
+            
             return response.json();
         },
+        refetchOnWindowFocus: false,
+        enabled: false
     });
-
-
-    if (isPending) return 'Loading...'
-
+    
+    const requestTexture = () => {
+        console.log("prompt " + prompt)
+        refetch();
+    }
+    
+    // if (isPending) return 'Loading...'
+    
     if (error) return 'An error has occurred: ' + error.message
 
     return(
