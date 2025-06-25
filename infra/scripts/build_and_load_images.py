dee5edd8021999d2247c667d7097699dc7db74c9
sethclim <56656904+sethclim@users.noreply.github.com>
2025-06-25 14:57:58 -0400

    added script to build images and load into KinD

---
+++ infra/scripts/build_and_load_images.py
@@ -0,0 +1,28 @@
+
+
+import subprocess
+
+
+from kind_load_image import load_image_kind
+
+base_path = "../../apps/"
+
+images = ["texture_engine", "texura_api"]
+
+def build_docker_image(name, path):
+    try:
+        res = subprocess.run(["docker", "build", "-t", name,  path], text=True)
+        print(res)
+
+    except subprocess.CalledProcessError as e:
+        print(f"Command failed with return code {e.returncode}")
+
+def main():
+    for image in images:
+        image_build_path = f"{base_path}/{image}"
+        build_docker_image(name=image, path=image_build_path)
+        load_image_kind(image)
+
+if __name__ == '__main__':
+    main()
+
